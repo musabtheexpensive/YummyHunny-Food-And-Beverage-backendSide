065d8d77280190a26f1a1ea28fda4d1916419b47
Musab Al Mahi <musabalmahi53@gmail.com>
2023-11-03 15:39:57 +0600

    specific one data click to show details to mongodb atlas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,14 @@ async function run() {
     app.get("/food", async (req, res) => {
       const cursor = foodCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // specific one data click to show details to mongodb atlas
+    app.get("/food/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
       res.send(result);
     });
 
